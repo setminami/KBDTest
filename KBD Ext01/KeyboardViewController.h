fde644d9e23e20400aa82d1170265cd0b383f7e9
setMinami <set.minami@gmail.com>
2016-03-22 01:48:16 +0900

    KBDテスト

---
+++ KBD Ext01/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  KBD Ext01
+//
+//  Created by SetMinami on 2016/03/22.
+//  Copyright © 2016年 setMinami Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
